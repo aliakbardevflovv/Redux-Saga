a15c9b2bdd9338db72488cfcfffb20f647b0dcf6
AliakbarETH <aliakbar.devsol@gmail.com>
2023-11-14 21:36:39 +0500

    Implemented persistent storage using redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
-import store from "./store";
+//import { persistor, store } from "./store/store";
+import store from './store/store';
+//import { PersistGate } from 'redux-persist/integration/react';
 
 
 const root = ReactDOM.createRoot(
@@ -13,7 +15,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <PersistGate  persistor={persistor}> */}
+        <App />
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
